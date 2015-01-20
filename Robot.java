1b2695580e9111abcc1dca70ac21edda0134466a
David Hunt <huntdog1541@gmail.com>
2015-01-20 00:18:05 -0600

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,32 @@
+
+
+
+public class Robot {
+	
+	int xposition;
+	int yposition;
+	int steps;
+	Map map;
+	
+	public Robot()
+	{
+		map = new Map();
+		xposition = 0;
+		yposition = 0;
+		steps = 0;
+	}
+	
+	public Robot(int x, int y)
+	{
+		map = new Map(x, y);
+		xposition = 0;
+		yposition = 0;
+		steps = 0;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Start");
+		Robot robby = new Robot();
+	}
+
+}
