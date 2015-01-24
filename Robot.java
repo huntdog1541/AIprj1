028e2ea037998649046556525644e23e62bd74bb
David Hunt <huntdog1541@gmail.com>
2015-01-23 22:41:04 -0600

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -7,10 +7,12 @@ public class Robot {
 	int yposition;
 	int steps;
 	Map map;
+	static Movement move;
 	
 	public Robot()
 	{
 		map = new Map();
+		move = new Movement(this, map);
 		xposition = 0;
 		yposition = 0;
 		steps = 0;
@@ -19,6 +21,7 @@ public class Robot {
 	public Robot(int x, int y)
 	{
 		map = new Map(x, y);
+		move = new Movement(this, map);
 		xposition = 0;
 		yposition = 0;
 		steps = 0;
@@ -28,6 +31,43 @@ public class Robot {
 		System.out.println("Hello Start");
 		Robot robby = new Robot();
 		//Graph gph = new Graph();
+		
+		
 	}
 
+	public int getXposition()
+	{
+		return xposition;
+	}
+	
+	public int getYposition()
+	{
+		return yposition;
+	}
+
+	public void setXposition(int xposition) {
+		this.xposition = xposition;
+	}
+
+	public void setYposition(int yposition) {
+		this.yposition = yposition;
+	}
+	
+	
+	public int getSteps() {
+		return steps;
+	}
+
+	public void setSteps(int steps) {
+		this.steps = steps;
+	}
+
+	public boolean checkAlive()
+	{
+		boolean ans = true;
+		if(map.checkAgent(xposition, yposition))
+			ans = false;
+		return ans;
+	}
+	
 }
