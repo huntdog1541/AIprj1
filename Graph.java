ee8e04728f49270ed1f45518a32796dad8fedd3a
David Hunt <huntdog1541@gmail.com>
2015-01-24 19:50:27 -0600

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -7,15 +7,42 @@ import javax.swing.JPanel;
 public class Graph {
 
 	JFrame frame;
+	Map map;
 	
-	public Graph()
+	public Graph(Map mps)
 	{
+		map = mps;
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new GridLayout(16, 16));
-		frame.add(new Pane());
+		//frame.add(new Pane());
+		updateGraph();
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	public Graph(Map mps, int xbound, int ybound)
+	{
+		map = mps;
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(xbound, ybound));
+		//frame.add(new Pane());
+		updateGraph();
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public void updateGraph()
+	{
+		int xbound = map.getXboundary(), ybound = map.getYboundary(), i = 0, j = 0;
+		for(i = 0; i < xbound; i++)
+		{
+			for(j = 0; j < ybound; j++)
+			{
+				frame.add(map.getPaneling(i, j));
+			}
+		}
 	}
 
 	public class Pane extends JPanel
@@ -26,12 +53,12 @@ public class Graph {
 		private static final long serialVersionUID = 1L;
 
 		public Pane(){
-		
+			this.setPreferredSize(getPreferredSize());
 		}
 		
 		public Dimension getPreferredSize()
 		{
-			return new Dimension(500, 500);
+			return new Dimension(500, 100);
 		}
 		
 		
