ad93bd53d676d9a327d951b542257832e74321d4
David Hunt <huntdog1541@gmail.com>
2015-01-25 17:05:08 -0600

    Create Results.java

---
+++ Results.java
@@ -0,0 +1,61 @@
+
+public class Results {
+
+	private Stats st;
+	private int currentRun;
+	private double percentWin;
+	private double percentLose;
+	private int totalRuns;
+	
+	
+	public Results()
+	{
+		st = new Stats();
+		currentRun = 1;
+		percentWin = 0.0;
+		percentLose = 0.0;
+		totalRuns = 1;
+	}
+
+
+	public Stats getSt() {
+		return st;
+	}
+
+	public void setSt(Stats st) {
+		this.st = st;
+	}
+
+	public int getCurrentRun() {
+		return currentRun;
+	}
+
+	public void setCurrentRun(int currentRun) {
+		this.currentRun = currentRun;
+	}
+
+	public double getPercentWin() {
+		return percentWin;
+	}
+
+	public void setPercentWin(double percentWin) {
+		this.percentWin = percentWin;
+	}
+
+	public double getPercentLose() {
+		return percentLose;
+	}
+
+	public void setPercentLose(double percentLose) {
+		this.percentLose = percentLose;
+	}
+
+	public int getTotalRuns() {
+		return totalRuns;
+	}
+
+	public void setTotalRuns(int totalRuns) {
+		this.totalRuns = totalRuns;
+	}
+	
+}
