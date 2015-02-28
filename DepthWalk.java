ff286b7c3f0b48bac3032eae52c97a4a873d649d
David Hunt <huntdog1541@gmail.com>
2015-02-28 01:11:20 -0600

    update

--- DepthWalk.java
+++ DepthWalk.java
@@ -40,18 +40,23 @@ public class DepthWalk {
 		xpost = map.getEntryX();
 		ypost = map.getEntryY();
         currentNode = new Node(xpost, ypost, map);
+        list.add(currentNode);
         currentIterator = 0;
 	}
 	
 	public void walking()
 	{
 		Node temp = list.get(0);
-        while(!temp.isTreasure())
+		boolean running = true;
+        while((!temp.isTreasure()) && (running == true))
         {
             addNext();
             list.remove(0);
             store.add(temp);
-            temp = list.get(0);
+            if(list.size() != 0)
+            	temp = list.get(0);
+            else
+            	running = false;
         }
         
         /*
@@ -66,7 +71,7 @@ public class DepthWalk {
 	
 	public void addNext()
 	{
-        int tempIterator = currentIterator, nwX, nwY, i;
+        //int tempIterator = currentIterator, nwX, nwY, i;
         Node temp = list.get(currentIterator);
         //Add different Nodes
         Node node1 = new Node(getNextX(temp, direction.WEST), getNextY(temp, direction.WEST), map);
@@ -98,12 +103,14 @@ public class DepthWalk {
     {
         if(map.isMoveBlocked(temp.getX(), temp.getY()))
             robby.increaseStep(temp.getX(), temp.getY());
-        if(map.isStandingOnAgent(temp.getY(), temp.getY()))
+        else if(map.isStandingOnAgent(temp.getY(), temp.getY()))
             robby.checkAlive();
-        if(map.hasStepTreasure(temp.getX(), temp.getY()))
+        else if(map.hasStepTreasure(temp.getX(), temp.getY()))
             robby.hasTreasure();
-        if(map.steppingOnEntry(temp.getX(), temp.getY()))
+        else if(map.steppingOnEntry(temp.getX(), temp.getY()))
             robby.atEntry();
+        else
+        	list.add(temp);
     }
 
 	//gets the next X value for the next square
@@ -138,26 +145,30 @@ public class DepthWalk {
     {
         int tempIterator1 = 0, tempIterator2 = 0;
         int oldX, oldY, nwX, nwY;
+        if(store.size() == 0)
+        	return;
         Node outerNode = store.get(tempIterator1);
         Node innerNode;
         boolean exit = false;
-        while((outerNode != null) || (tempIterator2 < store.size()) || (exit == false))
+        while((outerNode != null) && (tempIterator2 < store.size()) && (exit == false))
            {
              oldX = outerNode.getX();
              oldY = outerNode.getY();
              tempIterator2 = 0;
              innerNode = list.get(tempIterator2);
-             while((innerNode != null) || (tempIterator2 < list.size()) || (exit == false))
+             while((innerNode != null) && (tempIterator2 < list.size()) && (exit == false))
              {
-                if(outerNode.isMatch(innerNode.getX(), innerNode.getY())) {
+                if((outerNode.isMatch(innerNode.getX(), innerNode.getY())) && (tempIterator2 <= list.size())) {
                     list.remove(tempIterator2);
                     exit = true;
                  }
                 tempIterator2++;
-                innerNode = list.get(tempIterator2);
+                if(tempIterator2 < list.size())
+                	innerNode = list.get(tempIterator2);
             }
             tempIterator1++;
-            outerNode = store.get(tempIterator1);
+            if(tempIterator1 < store.size())
+            	outerNode = store.get(tempIterator1);
             }
     }
 }
